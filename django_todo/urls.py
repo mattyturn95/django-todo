8ad2743a17ca23918f57e158a48b85784179158d
mattyturn95 <mattyturn95@gmail.com>
2020-03-11 12:03:26 +0000

    changes

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
-from todo.views import get_todo_list
+from todo.views import get_todo_list, create_an_item
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', get_todo_list),
+    url(r'^add$', create_an_item)
 
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-
-
-def say_hello(request):
-    return HttpResponse("Hello World")
+from django.shortcuts import render, redirect
+from .models import Item
+from .forms import ItemForm
 
 
 def get_todo_list(request):
-    return render(request, "todo_list.html")
+    results = Item.objects.all()
+    return render(request, "todo_list.html", {'items': results})
+
+
+def create_an_item(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(get_todo_list)
+    else:
+        form = ItemForm()
+
+    return render(request, 'item_form.html', {'form': form})
