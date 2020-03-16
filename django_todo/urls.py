9b02f4e3dbdccf9431861892de5c66fd268d2102
mattyturn95 <mattyturn95@gmail.com>
2020-03-16 18:23:18 +0000

    changed

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -1,16 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
-from todo.views import get_todo_list, create_an_item, edit_an_item, toggle_status
+from todo.views import get_todo_list, create_an_item, edit_an_item,
+toggle_status
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', get_todo_list),
     url(r'^add$', create_an_item),
     url(r'^edit/(?P<id>\d+)$', edit_an_item),
-<<<<<<< HEAD
     url(r'^toggle/(?P<id>\d+)$', toggle_status)
-=======
-    url(r'^toggle/(?P<id>\d+)$', toggle_status),
->>>>>>> bbe5d520c6080644111e9fb9020c518575fab097
 
 ]
